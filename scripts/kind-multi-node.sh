e0869284e8bc4ec9bbe7e0c838a3401779e21555
devopsnov23 <150913274+devopsnov23@users.noreply.github.com>
2024-06-30 22:38:44 +0530

    Update kind-multi-node.sh

--- scripts/kind-multi-node.sh
+++ scripts/kind-multi-node.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-CLI=/usr/local/bin/kind
-sudo curl -Lo $CLI "https://kind.sigs.k8s.io/dl/v0.12.0/kind-$(uname)-amd64" && sudo chmod +x $CLI
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
 
 kind delete cluster > /dev/null 2>&1
 
@@ -33,5 +34,30 @@ sudo mv kubectl /usr/local/bin/
   kubectl get nodes && \
   kubectl cluster-info;
 } | grep -z 'Ready\| ok\|passed\|running'
+
+sudo apt install sipcalc -y
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
+cat > metal.yaml <<EOF
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: first-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  - 172.18.255.1-172.18.255.250 
+
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: example
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+  - first-pool
+EOF
+
+kubectl apply -f metal.yaml
 
 kubectl get nodes -o wide
